1b48a071ef336f5aff2e51c637946b3e96a1ca8f
starbal123 <sripadbal@gmail.com>
2025-09-15 13:20:31 +0530

    OngoingJobs API integrations

--- frontend/src/pages/Partner/OngoingJobs/OngoingJobs.js
+++ frontend/src/pages/Partner/OngoingJobs/OngoingJobs.js
@@ -22,6 +22,7 @@ import {
 } from "@ant-design/icons";
 import { API_CONST } from "../../../const";
 import { Link } from "react-router-dom";
+import { API_CONST } from "../../../const";
 
 const { Search } = Input;
 const { Title, Text, Paragraph } = Typography;
