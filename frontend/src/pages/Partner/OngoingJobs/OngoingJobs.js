aaa366483758328dd6158223db51e4faeb8bd9b5
saurab sen <saurav540@yahoo.com>
2025-09-21 22:46:02 -0700

    new changes

--- frontend/src/pages/Partner/OngoingJobs/OngoingJobs.js
+++ frontend/src/pages/Partner/OngoingJobs/OngoingJobs.js
@@ -24,6 +24,7 @@ import { Link } from "react-router-dom";
 import { API_CONST } from "../../../const";
 import { Link } from "react-router-dom";
 import { API_CONST } from "../../../const";
+import { Link } from "react-router-dom";
 
 const { Search } = Input;
 const { Title, Text, Paragraph } = Typography;
